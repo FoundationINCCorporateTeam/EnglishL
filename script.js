22295954c86a3b29b1483130dca049fab2ee7cf5
MN Industries <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-04-23 22:09:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,29 @@ function convertToHTML(input) {
     html = html.replace(/\[img\s+src="([^"]+)"\s+alt="([^"]+)"\s+width="([^"]+)"\s+height="([^"]+)"\]/g, '<img src="$1" alt="$2" width="$3" height="$4">');
 
     // Links with custom text
-    html = html.replace(/\[link\s+url="([^"]+)"\s+text="([^"]+)"\]/g, '<a href="$1">$2</a>');
+    html = html.replace(/\[link\s+url="([^"]+)"\](.*?)\[\.link\]/g, '<a href="$1">$2</a>');
+
+    // Headings
+    html = html.replace(/\[t([1-6])\](.*?)\[\.t\1\]/g, function(match, tag, content) {
+        return '<h' + tag + '>' + content + '</h' + tag + '>';
+    });
+
+    // Paragraphs
+    html = html.replace(/\[txt\](.*?)\[\.txt\]/g, '<p>$1</p>');
+
+    // Lists
+    html = html.replace(/\[unl\](.*?)\[\.unl\]/g, '<ul>$1</ul>');
+    html = html.replace(/\[orl\](.*?)\[\.orl\]/g, '<ol>$1</ol>');
+    html = html.replace(/\[lc\](.*?)\[\.lc\]/g, '<li>$1</li>');
+
+    // Tables
+    html = html.replace(/\[table\](.*?)\[\.table\]/g, '<table>$1</table>');
+    html = html.replace(/\[row\](.*?)\[\.row\]/g, '<tr>$1</tr>');
+    html = html.replace(/\[head\](.*?)\[\.head\]/g, '<th>$1</th>');
+    html = html.replace(/\[data\](.*?)\[\.data\]/g, '<td>$1</td>');
+
+    // Divisions
+    html = html.replace(/\[dv\](.*?)\[\.div\]/g, '<dv>$1</div>');
 
     return html;
 }
